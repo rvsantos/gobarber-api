c2d661a642aedff54562e6d8d14d7563342c1442
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-04-26 14:26:46 -0300

    Add express, typescript and ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.get("/", (request, response) => response.json({ message: "Hello Rafael" }));
+
+app.listen(3333, () => {
+  console.log(`Server started on port 3333...`);
+});
