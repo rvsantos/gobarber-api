08e80330be68dc374602f6a2e3fb375c0273ba70
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-04-26 20:36:38 -0300

    Settings prettier and eslint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express from 'express';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.get("/", (request, response) => response.json({ message: "Hello Rafael" }));
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log("Server started on port 3333...");
+  console.log('Server started on port 3333...');
 });
