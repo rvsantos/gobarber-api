0e6764e5b7d723a216c394b1211339ff09c00afd
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-04-26 14:58:27 -0300

    Add eslint

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ app.use(express.json());
 app.get("/", (request, response) => response.json({ message: "Hello Rafael" }));
 
 app.listen(3333, () => {
-  console.log(`Server started on port 3333...`);
+  console.log("Server started on port 3333...");
 });
