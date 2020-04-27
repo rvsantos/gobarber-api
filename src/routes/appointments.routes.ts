580e8050f3ba6fcced9fa7d9b485670aba2ae3c4
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-04-26 21:47:46 -0300

    Refactoring to add repository design pattern

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,19 +1,21 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+// repositories
+import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
+// variables
+const appointmentRepository = new AppointmentRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentInSameDate = appointmentRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate)
@@ -21,9 +23,7 @@ appointmentsRouter.post('/', (request, response) => {
       .status(400)
       .json({ error: 'This appointment is already booked.' });
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
