1e47a430a26a7d7b5f42299c9a7be2609778571a
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-05-02 22:27:36 -0300

    Add created_at and updated_at field to appointments table

--- src/database/migrations/1588031380612-CreateAppointments.ts
+++ src/database/migrations/1588031380612-CreateAppointments.ts
@@ -24,6 +24,16 @@ export default class CreateAppointments1588031380612
             type: 'timestamp with time zone',
             isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
