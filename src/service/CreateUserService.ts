866c39ff30d92095507b87192ea39e667ff812c7
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-05-03 20:48:52 -0300

    feat: add user credentials

--- src/service/CreateUserService.ts
+++ src/service/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 
 import User from '../models/User';
 
@@ -16,10 +17,12 @@ export default class CreateUserService {
 
     if (checkUserExists) throw new Error('Email address already used.');
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
